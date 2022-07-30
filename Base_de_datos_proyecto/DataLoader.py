416b79f406f60a15b3ca5fc8e57b22ba1f304beb
Rafther0112 <r.velasquez@uniandes.edu.co>
2022-07-30 00:33:06 -0500

    Enter

--- Base_de_datos_proyecto/DataLoader.py
+++ Base_de_datos_proyecto/DataLoader.py
@@ -33,18 +33,22 @@ def get_train_valid_loader(data_train_dir, data_valid_dir, batch_size,augment,ra
 
     return (train_loader, valid_loader)
 
-def get_test_loader(data_dir,batch_size,shuffle=True):
+def get_test_loader(data_test_dir,batch_size,shuffle=True):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225],)
 
     # define transform
     test_transform = transforms.Compose([transforms.Resize((227,227)),transforms.ToTensor(),normalize,])
 
-    test_dataset = AlzheimerDataset(csv_file = "test_data.csv", root_dir= data_dir, transform = test_transform)
+    test_dataset = AlzheimerDataset(csv_file = "test_data.csv", root_dir= data_test_dir, transform = test_transform)
     test_loader = DataLoader(dataset= test_dataset, batch_size=batch_size, shuffle=shuffle)
 
     return test_loader
 
 #Alzheimer Disease dataset 
-train_loader, valid_loader = get_train_valid_loader(data_dir = './data', batch_size = 64, augment = False, random_seed = 1)
+train_loader, valid_loader = get_train_valid_loader(data_train_dir= "train", data_valid_dir="validation", batch_size=32, augment=False, random_seed=True, shuffle=True)
 
-test_loader = get_test_loader(data_dir = './data', batch_size = 64)
+print(f"El dato de entrenamiento es: {train_loader}")
+print(f"El dato de validación es: {valid_loader}")
+
+test_loader = get_test_loader(data_test_dir="test", batch_size=32, shuffle=True)
+print(f"El dato de testeo es: {test_loader}")
