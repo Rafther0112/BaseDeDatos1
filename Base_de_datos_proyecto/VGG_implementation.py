66061ac42c483a6b48c4baa16280b55e66486166
Rafther0112 <r.velasquez@uniandes.edu.co>
2022-08-04 23:05:15 -0500

    Enter

---
+++ Base_de_datos_proyecto/VGG_implementation.py
@@ -0,0 +1,203 @@
+#%%
+import torch
+import torch.nn as nn
+from DataLoader import get_train_valid_loader, get_test_loader
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from train_model_VGG_implementation import train_model
+from graficador_VGG import plot_training_loss, plot_accuracy
+#%%
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#%%
+#Hyperparameters setting
+num_classes = 3
+num_epochs = 10
+batch_size = 64
+learning_rate = 0.005
+
+#%%
+# Model deep learning
+class VGG16(torch.nn.Module):
+
+    def __init__(self, num_classes):
+        super().__init__()
+        
+        self.block_1 = torch.nn.Sequential(
+                torch.nn.Conv2d(in_channels=3,
+                                out_channels=64,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(in_channels=64,
+                                out_channels=64,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool2d(kernel_size=(2, 2),
+                                   stride=(2, 2))
+        )
+        
+        self.block_2 = torch.nn.Sequential(
+                torch.nn.Conv2d(in_channels=64,
+                                out_channels=128,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(in_channels=128,
+                                out_channels=128,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool2d(kernel_size=(2, 2),
+                                   stride=(2, 2))
+        )
+        
+        self.block_3 = torch.nn.Sequential(        
+                torch.nn.Conv2d(in_channels=128,
+                                out_channels=256,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(in_channels=256,
+                                out_channels=256,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),        
+                torch.nn.Conv2d(in_channels=256,
+                                out_channels=256,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool2d(kernel_size=(2, 2),
+                                   stride=(2, 2))
+        )
+        
+          
+        self.block_4 = torch.nn.Sequential(   
+                torch.nn.Conv2d(in_channels=256,
+                                out_channels=512,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),        
+                torch.nn.Conv2d(in_channels=512,
+                                out_channels=512,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),        
+                torch.nn.Conv2d(in_channels=512,
+                                out_channels=512,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),            
+                torch.nn.MaxPool2d(kernel_size=(2, 2),
+                                   stride=(2, 2))
+        )
+        
+        self.block_5 = torch.nn.Sequential(
+                torch.nn.Conv2d(in_channels=512,
+                                out_channels=512,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),            
+                torch.nn.Conv2d(in_channels=512,
+                                out_channels=512,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),            
+                torch.nn.Conv2d(in_channels=512,
+                                out_channels=512,
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=1),
+                torch.nn.ReLU(),    
+                torch.nn.MaxPool2d(kernel_size=(2, 2),
+                                   stride=(2, 2))             
+        )
+            
+        height, width = 3, 3 ## you may want to change that depending on the input image size
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Linear(512*height*width, 4096),
+            torch.nn.ReLU(True),
+            torch.nn.Dropout(p=0.5),
+            torch.nn.Linear(4096, 4096),
+            torch.nn.ReLU(True),
+            torch.nn.Dropout(p=0.5),
+            torch.nn.Linear(4096, num_classes),
+        )
+            
+        for m in self.modules():
+            if isinstance(m, torch.torch.nn.Conv2d) or isinstance(m, torch.torch.nn.Linear):
+                torch.nn.init.kaiming_uniform_(m.weight, mode='fan_in', nonlinearity='relu')
+                if m.bias is not None:
+                    m.bias.detach().zero_()
+                    
+        self.avgpool = torch.nn.AdaptiveAvgPool2d((height, width))
+        
+        
+    def forward(self, x):
+
+        x = self.block_1(x)
+        x = self.block_2(x)
+        x = self.block_3(x)
+        x = self.block_4(x)
+        x = self.block_5(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1) # flatten
+        
+        logits = self.classifier(x)
+        #probas = F.softmax(logits, dim=1)
+
+        return logits               
+model = VGG16(num_classes=3)
+model = model.to(device)
+#%%
+# Loss and optimizer
+optimizer = torch.optim.SGD(model.parameters(), momentum=0.9, lr=0.01)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                       factor=0.1,
+                                                       mode='max',
+                                                       verbose=True)
+#Alzheimer Disease dataset 
+train_loader, valid_loader = get_train_valid_loader(valid_csv= "valid_data.csv", data_train_dir= "train", data_valid_dir="validation", batch_size=batch_size, augment=False, random_seed=True, shuffle=True)
+test_loader = get_test_loader(data_test_dir="test", batch_size=batch_size, shuffle=True)
+
+print(f"El dato de entrenamiento es: {train_loader}")
+print(f"El dato de validación es: {valid_loader}")
+print(f"El dato de testeo es: {test_loader}")
+
+#%%
+# Train the model
+minibatch_loss_list, train_acc_list, valid_acc_list = train_model(
+    model=model,
+    num_epochs=num_epochs,
+    train_loader=train_loader,
+    valid_loader=valid_loader,
+    test_loader=test_loader,
+    optimizer=optimizer,
+    device=device,
+    scheduler=scheduler,
+    scheduler_on='valid_acc',
+    logging_interval=100)
+
+
+plot_training_loss(minibatch_loss_list=minibatch_loss_list,num_epochs=num_epochs,iter_per_epoch=len(train_loader),results_dir=None,averaging_iterations=200)
+plt.show()
+
+plot_accuracy(train_acc_list=train_acc_list,
+              valid_acc_list=valid_acc_list,
+              results_dir=None)
+plt.ylim([60, 100])
+plt.show()
